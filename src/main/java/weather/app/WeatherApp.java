fa9504ff8bd1a5fe46ece341dae97ae96fd7029a
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-04-19 00:06:45 +0700

    test retrieving forecast

--- src/main/java/weather/app/WeatherApp.java
+++ src/main/java/weather/app/WeatherApp.java
@@ -2,6 +2,7 @@ package weather.app;
 import backend.apicon;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class WeatherApp {
@@ -13,13 +14,18 @@ public class WeatherApp {
             double temp = apicon.currentTemp(country);
             System.out.println("Current temperature in " + country + "= " + temp);
             
+            JSONArray forecastHourly = apicon.getHourlyForecast(country); 
+            System.out.println("Today's Temperature in " + country + " at 10PM: " + forecastHourly.get(19) );
+            
+            JSONArray forecastDaily = apicon.getDailyForecast(country); 
+            System.out.println("Temperature in 5 days from today in " + country + " : " + forecastDaily.get(4) );
+            
             // throw exception when country not found
             temp = apicon.currentTemp("england");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         
-       
 
     }
 }
