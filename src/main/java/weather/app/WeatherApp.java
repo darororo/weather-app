2e7d7521edd705605507c9eae370bba37c376dcf
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-04-18 15:32:45 +0700

    test a method from apicon class

--- src/main/java/weather/app/WeatherApp.java
+++ src/main/java/weather/app/WeatherApp.java
@@ -1,17 +1,25 @@
 package weather.app;
 import backend.apicon;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONObject;
 
 public class WeatherApp {
 
-    public static void main(String[] args) {
-        JSONObject country = apicon.getCountry("cambodia");
-        if (country == null) {
-            System.out.println("Country not found");
-            return;
-        }
-        JSONObject weather = apicon.getWeather(country);
-        System.out.println(apicon.currentTemp(weather));
+    public static void main(String[] args){
         
+        try {
+            String country = "Cambodia";
+            double temp = apicon.currentTemp(country);
+            System.out.println("Current temperature in " + country + "= " + temp);
+            
+            // throw exception when country not found
+            temp = apicon.currentTemp("england");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+       
+
     }
 }
