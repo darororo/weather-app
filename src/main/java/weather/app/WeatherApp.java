4de607032483c12a14632707fcbfabf0283d36c6
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-04-19 15:58:58 +0700

    move test code to test package

--- src/main/java/weather/app/WeatherApp.java
+++ src/main/java/weather/app/WeatherApp.java
@@ -9,23 +9,23 @@ public class WeatherApp {
 
     public static void main(String[] args){
         
-        try {
-            String country = "Cambodia";
-            double temp = apicon.currentTemp(country);
-            System.out.println("Current temperature in " + country + "= " + temp);
-            
-            JSONArray forecastHourly = apicon.getHourlyForecast(country); 
-            System.out.println("Today's Temperature in " + country + " at 10PM: " + forecastHourly.get(19) );
-            
-            JSONArray forecastDaily = apicon.getDailyForecast(country); 
-            System.out.println("Temperature in 5 days from today in " + country + " : " + forecastDaily.get(4) );
-            
-            // throw exception when country not found
-            temp = apicon.currentTemp("england");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        
+//        try {
+//            String country = "Cambodia";
+//            double temp = apicon.currentTemp(country);
+//            System.out.println("Current temperature in " + country + "= " + temp);
+//            
+//            JSONArray forecastHourly = apicon.getHourlyForecast(country); 
+//            System.out.println("Today's Temperature in " + country + " at 10PM: " + forecastHourly.get(19) );
+//            
+//            JSONArray forecastDaily = apicon.getDailyForecast(country); 
+//            System.out.println("Temperature in 5 days from today in " + country + " : " + forecastDaily.get(4) );
+//            
+//            // throw exception when country not found
+//            temp = apicon.currentTemp("england");
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        
 
     }
 }
