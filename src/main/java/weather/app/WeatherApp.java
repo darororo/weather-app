af287dad136da08a0332894ca9ca8255c393bc02
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-04-21 11:01:34 +0700

    uses javafx functionalities

--- src/main/java/weather/app/WeatherApp.java
+++ src/main/java/weather/app/WeatherApp.java
@@ -1,13 +1,41 @@
 package weather.app;
-import backend.apicon;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
-public class WeatherApp {
+import backend.apicon;
+import java.io.IOException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+
+public class WeatherApp extends Application{
+    
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("view-sunny"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(WeatherApp.class.getResource("/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+   
 
     public static void main(String[] args){
+        launch();
         
 //        try {
 //            String country = "Cambodia";
@@ -28,4 +56,5 @@ public class WeatherApp {
 //        
 
     }
+
 }
