291ce5a9a91908e918ed4c46d81681781684ce11
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-04-22 22:39:44 +0700

    testing new methods from apicon class

--- src/test/java/WeatherAppTest.java
+++ src/test/java/WeatherAppTest.java
@@ -1,35 +1,24 @@
-//import backend.apicon;
-//import java.io.IOException;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import javafx.application.Application;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//
-//public class WeatherAppTest extends Application{
-//    
-//    private static Scene scene;
-//
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        scene = new Scene(loadFXML("view-sunny"), 640, 480);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//    
-//    private static Parent loadFXML(String fxml) throws IOException {
-//        String root = "E:\\java-project\\weather-app\\src\\main\\java\\frontend\\";
-//        FXMLLoader fxmlLoader = new FXMLLoader(WeatherAppTest.class.getResource( root + fxml + ".fxml"));
-//        return fxmlLoader.load();
-//    }
-//
-//    public static void main(String[] args){
-//        
-//        try {
+import backend.apicon;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class WeatherAppTest {
+    
+
+    public static void main(String[] args){
+        
+        try {
+            apicon w = new apicon("japan"); 
+            double t1 = w.currentTemp();
+            System.out.println("Today in " + w.getCountryName() + " is " + w.getCurrentDate() );
+            System.out.println("Current time in " + w.getCountryName() + " : " + w.getCurrentTime() );
+            System.out.println("Current temperature in " + w.getCountryName() + "= " + t1);
+            System.out.println("Today's Temperature in " + w.getCountryName() + " at 10PM: " + w.getHourlyForecast().get(19) );
+            System.out.println("Temperature in 5 days from today in " + w.getCountryName() + " : " + w.getDailyForecast().get(4) );
+
 //            String country = "Cambodia";
 //            double temp = apicon.currentTemp(country);
 //            System.out.println("Current temperature in " + country + "= " + temp);
@@ -42,12 +31,13 @@
 //            
 //            // throw exception when country not found
 //            temp = apicon.currentTemp("england");
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//        
-//
-//    }
-//
-//
-//}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        
+
+    }
+
+
+}
